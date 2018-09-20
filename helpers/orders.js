40c2705e551832f841e90a9232971f2362100b91
MarkNewcomb1 <wlmmn@hotmail.com>
2018-09-20 15:21:59 -0600

    added formatting helper functions for prettifying the data

--- helpers/orders.js
+++ helpers/orders.js
@@ -10,12 +10,81 @@ exports.getOrder = function(req, res) {
     });
 };
 exports.createOrder = function(req, res) {
-  db.Order.create(req.body)
+  
+  db.Order.create(prettifyValues(req.body))
     .then(function(saved) {
+      console.log("SAVED", saved);
+      
       res.status(201).json(saved);
     })
     .catch(function(err) {
       res.send(err);
     });
 };
+
+function prettifyValues(guitar) {
+  for (var key in guitar) {
+    switch(key) {
+      case 'body':
+        guitar.body = prettifyBody(guitar.body);
+        break;
+      case 'hand':
+        guitar.hand = prettifyHand(guitar.hand);
+        break;
+      case 'paint':
+        guitar.paint = prettifyPaint(guitar.paint);
+        break;
+    }
+  }
+  return guitar;
+}
+
+function prettifyBody(body) {
+  return body === 'strat' ? 'Fender Stratocaster' : "Gibson Les Paul";
+}
+
+function prettifyHand(hand) {
+  return hand === 'left' ? 'Left-Handed' : 'Right-Handed';
+}
+
+function prettifyPaint(paint) {
+  switch(paint) {
+    case 'maroon':
+      return 'Maroon';
+      break;
+    case '#ADCEBE':
+      return 'Surf Green';
+      break; 
+    case '#fffff6':
+      return 'White';
+      break;   
+    case 'purple':
+      return 'Purple';
+      break;  
+    case 'black':
+      return 'Black';
+      break;
+    case 'url(#metallic-teal)':
+      return 'Metallic Teal';
+      break;
+    case 'url(#metallic-blue)':
+      return 'Metallic Blue';
+      break;
+    case 'url(#metallic-burnt-orange)':
+      return 'Metallic Burnt Orange';
+      break;
+    case 'url(#metallic-gold)':
+      return 'Metallic Gold';
+      break;
+    case 'url(#metallic-gray)':
+      return 'Metallic Gray';
+      break;
+    case 'url(#metallic-purple)':
+      return 'Metallic Purple';
+      break;
+    case 'url(#clear)':
+      return 'Clear';
+      break;
+  }
+}
 module.exports = exports;
